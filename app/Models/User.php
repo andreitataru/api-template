434ebe76e342a6f3f811e7ecfe1ae099050e9c53
Andrei Tataru <andreitataru.at@gmail.com>
2021-04-06 11:14:58 +0100

    env

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,7 +23,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var array
      */
     protected $fillable = [
-        'name', 'email'
+        'name', 'email', 'password'
     ];
 
     /**
