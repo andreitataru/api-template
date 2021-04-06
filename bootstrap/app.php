746052a801a5f9c5f0646d7e18a952e5ec7aa260
Andrei Tataru <andreitataru.at@gmail.com>
2021-04-06 09:25:48 +0100

    cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -80,6 +80,7 @@ $app->configure('app');
 
 $app->routeMiddleware([
     'auth' => App\Http\Middleware\Authenticate::class,
+    'cors' => App\Http\Middleware\CorsMiddleware::class
 ]); 
 
 /*
--- routes/web.php
+++ routes/web.php
@@ -17,23 +17,29 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
+$router->group(['middleware' => 'cors'], function () use ($router) {
+    //All the routes you want to allow CORS for
+  
+    $router->options('/{any:.*}', function (Request $req) {
+      return;
+    });
 
+    // API route group
+    $router->group(['prefix' => 'api'], function () use ($router) {
+        // Matches "/api/register
+    $router->post('register', 'AuthController@register');
 
-// API route group
-$router->group(['prefix' => 'api'], function () use ($router) {
-    // Matches "/api/register
-   $router->post('register', 'AuthController@register');
+        // Matches "/api/login
+        $router->post('login', 'AuthController@login');
 
-     // Matches "/api/login
-    $router->post('login', 'AuthController@login');
+        // Matches "/api/profile
+        $router->get('profile', 'UserController@profile');
 
-    // Matches "/api/profile
-    $router->get('profile', 'UserController@profile');
+        // Matches "/api/users/1 
+        //get one user by id
+        $router->get('users/{id}', 'UserController@singleUser');
 
-    // Matches "/api/users/1 
-    //get one user by id
-    $router->get('users/{id}', 'UserController@singleUser');
-
-    // Matches "/api/users
-    $router->get('users', 'UserController@allUsers');
+        // Matches "/api/users
+        $router->get('users', 'UserController@allUsers');
+    });
 });
